f42495d4149149a8472f80d6f082e8b1434acdf4
Francis Hitchens <fhitchen@gmail.com>
2019-12-29 10:41:54 -0600

    Removed unneeded port remappings and action badge.

--- test/earthen/clj_cb/fixtures.clj
+++ test/earthen/clj_cb/fixtures.clj
@@ -34,30 +34,9 @@
   [username password]
   (c/authenticate @cluster username password))
 
-(defn- get-cluster-from-env
-  "Used in github action where port is defined in job.services.couchbase.ports['11210']"
-  []
-  (let [http-port (System/getenv "HTTP_DIRECT_PORT")
-        carrier-port (System/getenv "CARRIER_DIRECT_PORT")]
-    (System/setProperty "com.couchbase.disconnectTimeout" "60000")
-    (if (some? http-port)
-      (do
-        (println "Setting " http-port carrier-port)
-        (System/setProperty "com.couchbase.NetworkResolution" "external")
-        (System/setProperty "com.couchbase.bootstrapHttpEnabled" "true")
-        (System/setProperty "com.couchbase.bootstrapHttpDirectPort" http-port)
-        (System/setProperty "com.couchbase.bootstrapCarrierDirectPort" carrier-port)
-        "127.0.0.1")
-      "127.0.0.1")))
-
-
 (defn init
   [f]
-  (let [host (get-cluster-from-env)]
-    (prn "HOSTNAME is: " host)
-    (if (some? host)
-      (reset! cluster (c/create host))
-      (reset! cluster (c/create))))
+  (reset! cluster (c/create))
   (c/remove-bucket! (manager) bucket-name)
   (c/insert-bucket! (manager) default-bucket-settings)
   (f)
